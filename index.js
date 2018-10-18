b6534205c03dc6b6453d941d978a380b29c7fb73
hillrg22 <hillrg22@gmail.com>
2018-10-18 14:39:11 -0600

    added cors and bodyParser dependencies, wrote first  app.get

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const people = require ('./people')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+
 
 app.listen(port,() =>{
   console.log(`I got you on ${port}`)
+})
+
+
+app.get('/', (req,res,next) =>{
+  res.send('Hi there')
 })
